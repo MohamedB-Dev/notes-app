ac9ef9c727038edc306a74e0b07af4ddf2357640
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-09-25 18:26:03 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './components/posts/posts.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
