c2a79e6aeaf93945137de5f286a1455fa2ae8603
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-09-26 16:13:56 +0300

    edit AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
+// Components
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PostsComponent } from './components/posts/posts.component';
+import { NotesComponent } from './components/notes/notes.component';
 import { ListComponent } from './components/list/list.component';
+
+// Angular-Material
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule  } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+
+// Services
+import { NotesService } from './services/note.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
+    NotesComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    DragDropModule
   ],
-  providers: [],
+  
+  providers: [
+    NotesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
