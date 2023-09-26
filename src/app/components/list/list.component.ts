d35026c0b89a61f33c6d5e9007ec350efd576aa6
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-09-26 16:14:08 +0300

    add ListComponent

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Note } from '../../models/note.model';
+import { NotesService } from '../../services/note.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
 
+export class ListComponent implements OnInit, OnDestroy {
+  notes: Note[] =[];
+  private notesSub: Subscription;
+
+  constructor(public notesService: NotesService) { }
+
+  ngOnInit(): void {
+    this.notes = this.notesService.getNote();
+    this.notesSub = this.notesSub = this.notesService.getNoteUpdateListener()
+    .subscribe((notes:Note[])=>{
+      this.notes=notes;
+    }); 
+  }
+
+  ngOnDestroy(): void {
+    this.notesSub.unsubscribe();
+  }
+
+  editNote(note){
+    this.notesService.editNote(note);
+  }
+
+  deleteNote(note){
+    this.notesService.deleteNote(note);
+  }
 }
