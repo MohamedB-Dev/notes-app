879feb7b9b4125aa18ec8eb9ce18cca08ec7dcc3
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-09-28 00:26:57 +0300

    add new functions for del, edit and save for the notes

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -10,28 +10,41 @@ import { Subscription } from 'rxjs';
 })
 
 export class ListComponent implements OnInit, OnDestroy {
-  notes: Note[] =[];
+  notes: Note[] = [];
   private notesSub: Subscription;
+  editStates: { [noteId: string]: boolean } = {};
+  editedTitles: { [noteId: string]: string } = {};
+  editedDescriptions: { [noteId: string]: string } = {};
 
   constructor(public notesService: NotesService) { }
 
   ngOnInit(): void {
-    this.notes = this.notesService.getNote();
-    this.notesSub = this.notesSub = this.notesService.getNoteUpdateListener()
-    .subscribe((notes:Note[])=>{
-      this.notes=notes;
-    }); 
+    this.notesService.getNote();
+    this.notesSub = this.notesService.getNoteUpdateListener()
+      .subscribe((notes: Note[]) => {
+        this.notes = notes;
+      });
+  }
+
+  onEdit(noteId: string) {
+    this.editStates[noteId] = true;
+    this.editedTitles[noteId] = this.notes.find(note => note.noteId === noteId)?.title || '';
+    this.editedDescriptions[noteId] = this.notes.find(note => note.noteId === noteId)?.description || '';
+  }
+
+  onSave(noteId: string) {
+    this.notesService.editNotes(noteId, this.editedTitles[noteId], this.editedDescriptions[noteId]);
+    this.editStates[noteId] = false;
+    this.notes.find(note => note.noteId === noteId).title = this.editedTitles[noteId];
+    this.notes.find(note => note.noteId === noteId).description = this.editedDescriptions[noteId];
+  }
+
+  onDelete(noteId: string) {
+    this.notesService.deleteNote(noteId);
   }
 
   ngOnDestroy(): void {
     this.notesSub.unsubscribe();
   }
 
-  editNote(note){
-    this.notesService.editNote(note);
-  }
-
-  deleteNote(note){
-    this.notesService.deleteNote(note);
-  }
 }
--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,31 +1,67 @@
 import { Note } from '../models/note.model';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 export class NotesService {
-    private notes: Note[] = [];
-    private notesUpdated = new Subject<Note[]>();
+  private notes: Note[] = [];
+  private notesUpdated = new Subject<Note[]>();
 
-    getNote() {
-        return [...this.notes];
-    }
+  constructor(private http: HttpClient) { }
 
-    getNoteUpdateListener() {
-        return this.notesUpdated.asObservable();
-    }
-    
-    addNote(title: string, description: string) {
-        const note: Note = {title: title,description: description};
+  getNote() {
+    this.http.get<{ message: string; data: any[] }>('http://localhost:3000/notes')
+      .pipe(map((response) => {
+        return response.data.map(item => {
+          return {
+            noteId: item._id,
+            title: item.title,
+            description: item.description,
+          };
+        });
+      }))
+      .subscribe((noteData: Note[]) => {
+        console.log(noteData);
+        this.notes = noteData;
+        this.notesUpdated.next([...this.notes]);
+      });
+  }
+
+
+  addNotes(title: string, description: string) {
+    const note: Note = { noteId: null, title: title, description: description };
+    this.http.post<{ message: string; noteId: string }>('http://localhost:3000/notes', note)
+      .subscribe((responseData) => {
+        const id = responseData.noteId;
+        note.noteId = id;
         this.notes.push(note);
         this.notesUpdated.next([...this.notes]);
+      });
+  }
 
-    }
+  editNotes(noteId: string, title: string, description: string) {
+    const note: Note = { noteId: noteId, title: title, description: description };
+    this.http.put('http://localhost:3000/notes/' + noteId, note)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 
-    editNote(note){
-        this.notesUpdated.next(note);
-    }
+  deleteNote(noteId: string) {
+    this.http.delete('http://localhost:3000/notes/' + noteId)
+      .subscribe(() => {
+        const updatedNotes = this.notes.filter(note => note.noteId !== noteId);
+        this.notes = updatedNotes;
+        this.notesUpdated.next([...this.notes]);
+      });
+  }
 
-    deleteNote(note){
-        this.notesUpdated.next(note);
-    }
-
+  getNoteUpdateListener() {
+    return this.notesUpdated.asObservable();
+  }
 }
