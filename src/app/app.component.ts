5572f4dd98dc91ba0b6a32293c49639f592096db
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-11-13 03:06:12 +0300

    edit the app Component, after add the sign/loing pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'notes-app';
-  username = 'Mohamed Adnan';
+export class AppComponent implements OnInit {
+  private authListenerSubs: Subscription;
+  userIsAuthenticated = false;
+  username: string;
+
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(){
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+      this.username = this.authService.getUsername();
+    });
+
+    this.authService.autoAuthUser();
+
+  }
+
+  onLougout(){
+    this.authService.logout();
+    this.username = null;
+  }
+
+  ngOnDestroy(){
+    this.authListenerSubs.unsubscribe();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
@@ -16,18 +16,26 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {MatIconModule} from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { AppRoutingModule } from './app-router.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Services
-import { NotesService } from './services/note.service';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     WritingNotesComponent,
-    NoteListComponent
+    NoteListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     FormsModule,
     MatInputModule,
@@ -39,9 +47,7 @@ import { NotesService } from './services/note.service';
     MatPaginatorModule,
   ],
   
-  providers: [
-    NotesService
-  ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
