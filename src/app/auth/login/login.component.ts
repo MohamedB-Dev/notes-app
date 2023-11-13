b0f24e2eaf38e626e24c34f03cecd14f3b349ee4
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-11-13 03:05:08 +0300

    add login page

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authService.login(
+      this.loginForm.value.email,
+      this.loginForm.value.password
+    );
+  }
+}
